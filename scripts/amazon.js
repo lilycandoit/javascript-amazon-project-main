ef49f989108b12193327159b95c59371c0f4a723
lilycandoit <119943777+lilycandoit@users.noreply.github.com>
2023-12-22 03:29:20 +0000

    update cart with quantity selector

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -25,7 +25,7 @@ products.forEach((product) => {
       </div>
 
       <div class="product-quantity-container">
-        <select>
+        <select class="js-quantity-selector-${product.id}">
           <option selected value="1">1</option>
           <option value="2">2</option>
           <option value="3">3</option>
@@ -70,12 +70,16 @@ document.querySelectorAll('.js-add-to-cart').forEach((button) => {
       }
     });
 
+    const quantitySelector = document.querySelector(`.js-quantity-selector-${productId}`);
+
+    const quantitySelected = Number(quantitySelector.value);
+
     if (matchingItem) {
-      matchingItem.quantity += 1;
+      matchingItem.quantity += quanitySelected;;
     } else {
       cart.push({
         productId: productId,
-        quantity: 1,
+        quantity: quantitySelected,
       });
     }
 
@@ -86,9 +90,6 @@ document.querySelectorAll('.js-add-to-cart').forEach((button) => {
     });
 
     document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
-
-    console.log(cartQuantity);
-    console.log(cart);
   });
 });
 
