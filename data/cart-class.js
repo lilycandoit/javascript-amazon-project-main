614d507e64ba716db282dbde43f1956c9c2e508d
lilycandoit <hueduong288@gmail.com>
2024-04-30 00:49:50 +0800

    create function/ class to generate objects -  OOP (in cart)

---
+++ data/cart-class.js
@@ -0,0 +1,122 @@
+//other than function, class is better to generate object oriented programing language. it goes along with features of constructor
+
+class Cart {
+  cartItems; // same as: cartItems = undefined;
+  localStorageKey; //same as: localStorageKey = undefined;
+
+  constructor(key) {
+    //it's function, can use param "key" to make it dynamic
+    this.localStorageKey = key;
+    this.loadFromStorage();
+  }
+
+  loadFromStorage() {
+    this.cartItems = JSON.parse(localStorage.getItem(this.localStorageKey));
+
+    if (!this.cartItems) {
+      this.cartItems = [
+        {
+          productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+          quantity: 2,
+          deliveryOptionId: '1',
+        },
+        {
+          productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+          quantity: 2,
+          deliveryOptionId: '2',
+        },
+      ];
+    }
+  }
+
+  saveToStorage() {
+    localStorage.setItem(this.localStorageKey, JSON.stringify(this.cartItems));
+  }
+
+  addToCart(productId) {
+    let matchingItem;
+
+    this.cartItems.forEach((cartItem) => {
+      if (productId === cartItem.productId) {
+        matchingItem = cartItem;
+      }
+    });
+
+    // const quantitySelector = document.querySelector(
+    //   `.js-quantity-selector-${productId}`
+    // );
+
+    // const quantitySelected = Number(quantitySelector.value);
+
+    if (matchingItem) {
+      matchingItem.quantity += 1;
+    } else {
+      this.cartItems.push({
+        productId, // shortcut of the below
+        // productId: productId,
+        quantity: 1, //quantitySelected
+        deliveryOptionId: '1', // as a initial default option
+      });
+    }
+
+    this.saveToStorage();
+  }
+
+  removeFromCart(productId) {
+    const newCart = [];
+
+    this.cartItems.forEach((cartItem) => {
+      if (cartItem.productId !== productId) {
+        newCart.push(cartItem);
+      }
+    });
+
+    this.cartItems = newCart;
+    this.saveToStorage();
+  }
+
+  calculateCartQuantity() {
+    let cartQuantity = 0;
+
+    this.cartItems.forEach((cartItem) => {
+      cartQuantity += cartItem.quantity;
+    });
+
+    return cartQuantity;
+  }
+
+  updateCart(productId, newQuantity) {
+    let matchingItem;
+
+    this.cartItems.forEach((cartItem) => {
+      if (productId === cartItem.productId) {
+        matchingItem = cartItem;
+      }
+    });
+
+    matchingItem.quantity = newQuantity;
+
+    this.saveToStorage();
+  }
+
+  updateDeliveryOption(productId, deliveryOptionId) {
+    let matchingItem;
+
+    this.cartItems.forEach((cartItem) => {
+      if (productId === cartItem.productId) {
+        matchingItem = cartItem;
+      }
+    });
+
+    matchingItem.deliveryOptionId = deliveryOptionId;
+
+    this.saveToStorage();
+  }
+}
+
+const cart = new Cart('cart-oop');
+
+const businessCart = new Cart('cart-business');
+
+console.log(cart);
+console.log(businessCart);
--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,7 +1,10 @@
-
 import { renderCheckoutHeader } from './checkout/checkoutHeader.js';
 import { renderOrderSummary } from './checkout/orderSummary.js';
 import { renderPaymentSummary } from './checkout/paymentSummary.js';
+
+// practice OOP (object oriented programing)
+// import '../data/cart-oop.js';
+import '../data/cart-class.js';
 
 renderCheckoutHeader();
 renderOrderSummary();
