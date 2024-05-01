a9550d2ff731ca4aafc1d6be32acb34afb7b33b5
lilycandoit <hueduong288@gmail.com>
2024-05-01 19:41:45 +0800

    done 17c,d

--- lesson 17_exercises/car.js
+++ lesson 17_exercises/car.js
@@ -1,15 +1,50 @@
 class Car {
   brand;
   model;
+  speed = 0;
+  isTrunkOpen = false;
 
   constructor(carInfo) {
     this.brand = carInfo.brand;
     this.model = carInfo.model;
-    // this.displayInfo();
   }
 
   displayInfo() {
-    return `${this.brand} ${this.model}`;
+    const trunkStatus = this.isTrunkOpen ? 'open' : 'closed';
+    return `${this.brand} ${this.model}, Speed: ${this.speed} km/h, Trunk: ${trunkStatus}`;
+  }
+
+  go() {
+    if (!this.isTrunkOpen) {
+      this.speed += 5;
+    }
+
+    //limit the speed to 200:
+    if (this.speed > 200) {
+      this.speed = 200;
+    }
+
+    return this.speed;
+  }
+
+  brake() {
+    this.speed -= 5;
+
+    //limit speed to 0
+    if (this.speed < 0) {
+      this.speed = 0;
+    }
+    return this.speed;
+  }
+
+  openTrunk() {
+    if (this.speed === 0) {
+      this.isTrunkOpen = true;
+    }
+  }
+
+  closeTrunk() {
+    this.isTrunkOpen = false;
   }
 }
 
@@ -20,4 +55,4 @@ const cars = [
   return new Car(car);
 });
 
-cars.forEach((car) => console.log(car.displayInfo()));
+cars.forEach((car) => console.log(car.displayInfo(), car.go(), car.brake()));
