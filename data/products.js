7f373c34b69a0e8776a82acdf6269190f3c200ab
lilycandoit <hueduong288@gmail.com>
2024-05-25 14:54:54 +0700

    load products from backend

--- data/products.js
+++ data/products.js
@@ -76,6 +76,33 @@ class Appliance extends Product {
   }
 }
 
+export let products = [];
+
+export function loadProducts(fun) {
+  const xhr = new XMLHttpRequest();
+
+  xhr.addEventListener('load', () => {
+    products = JSON.parse(xhr.response).map((productDetails) => {
+      if (productDetails.type === 'clothing') {
+        return new Clothing(productDetails);
+      }
+      if (productDetails.keywords.includes('appliances')) {
+        return new Appliance(productDetails);
+      }
+      return new Product(productDetails);
+    });
+
+    console.log('products loaded', products);
+
+    //only after loading the response of request, we will run the function name fun
+    fun();
+  });
+
+  xhr.open('GET', 'https://supersimplebackend.dev/products');
+  xhr.send();
+}
+
+/*
 export const products = [
   {
     id: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
@@ -556,5 +583,4 @@ export const products = [
   }
   return new Product(productDetails);
 });
-
-// console.log(products);
+*/
--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,12 +1,19 @@
 import { renderCheckoutHeader } from './checkout/checkoutHeader.js';
 import { renderOrderSummary } from './checkout/orderSummary.js';
 import { renderPaymentSummary } from './checkout/paymentSummary.js';
+import { loadProducts } from '../data/products.js';
 
 // practice OOP (object oriented programing)
 // import '../data/cart-oop.js';
 // import '../data/cart-class.js';
-import '../data/backend_practice.js';
+//import '../data/backend_practice.js';
 
-renderCheckoutHeader();
-renderOrderSummary();
-renderPaymentSummary();
+loadProducts(() => {
+  renderCheckoutHeader();
+  renderOrderSummary();
+  renderPaymentSummary();
+});
+
+// note1: the param of FUNCION loadProducts other than variable/ name/ a function name, it can also be anonymous function - which is the function without name.
+
+// note2: setTimeOut is another example of Call back function
